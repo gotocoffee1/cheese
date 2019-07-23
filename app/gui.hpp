929f215ad77955899e2b344c0da089962d6af2e7
bytesloth <oneminecraft1@googlemail.com>
2019-07-23 18:26:03 +0200

    fixed numbers

--- app/gui.hpp
+++ app/gui.hpp
@@ -18,16 +18,18 @@ public:
 	console_gui(chess_board cb) : gui(cb) {}
 
 	void print_board() {
-		string border_helper = "";
+		string number_helper = "   ";
+		string border_helper = "   ";
 		string white_field = "|||||";
 		string empty_white_field = "|| ||";
 		string black_field = "     ";
-		string white_black_line = "|";
-		string white_black_line_empty = "|";
-		string black_white_line = "|";
-		string black_white_line_empty = "|";
+		string white_black_line = "  |";
+		string white_black_line_empty = " |";
+		string black_white_line = "  |";
+		string black_white_line_empty = " |";
 		for (int i = 1; i < 9; i++) {
-			border_helper += "--" + to_string(i) + "--";
+			number_helper += "  " + to_string(i) + "  ";
+			border_helper += "-----";
 			if (i % 2 == 1) {
 				white_black_line += white_field;
 				white_black_line_empty += empty_white_field;
@@ -44,28 +46,29 @@ public:
 		string rim = "| \n";
 		string whole_white_black_line = "";
 		string whole_black_white_line = "";
-
-		for (int j = 8; j > 0; j--) {
+		string board = "";
+		for (int j = 8; j > 0; j = j - 2) {
+			whole_white_black_line = "";
+			whole_black_white_line = "";
 			for (int i = 0; i < 3; i++) {
 				if (i != 1) {
 					whole_white_black_line += white_black_line + rim;
 					whole_black_white_line += black_white_line + rim;
 				}
 				else {
-					whole_white_black_line += white_black_line_empty + "| " + to_string(j) "\n";
-					whole_black_white_line += black_white_line_empty + rim;
+					whole_white_black_line += to_string(j) + white_black_line_empty + "| " + to_string(j) + "\n";
+					whole_black_white_line += to_string(j - 1) + black_white_line_empty + "| " + to_string(j - 1) + "\n";
 				}
 			}
-		}
-
-
-		string board = "";
-		for (int i = 0; i < 4; i++) {
 			board += whole_white_black_line + whole_black_white_line;
 		}
 
-		cout << "|" << border_helper << "| \n";
+
+		
+		cout << number_helper << "\n";
+		cout << border_helper << "\n";
 		cout << board;
-		cout << "|" << border_helper << "| \n";
+		cout << border_helper << "\n";
+		cout << number_helper << "\n";
 	}
 };
