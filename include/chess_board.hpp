b833006499d3bbb484dc43113840b6e4ba6bd466
bytesloth <oneminecraft1@googlemail.com>
2019-07-25 18:07:37 +0200

    move right piece with right color

--- include/chess_board.hpp
+++ include/chess_board.hpp
@@ -47,10 +47,59 @@ public:
 	void move(int stx, int sty, int destx, int desty)
 	{
 		cout << "is_move_valid " << (*board_field[sty][stx].get_piece()).is_move_valid(stx, sty, destx, desty) << "\n";
-		cout << "piece " << (*board_field[sty][stx].get_piece()).get_short_name() << "\n";
+		string short_name = (*board_field[sty][stx].get_piece()).get_short_name();
+		cout << "piece " << short_name << "\n";
+		color col = (*board_field[sty][stx].get_piece()).c;
 		if ((*board_field[sty][stx].get_piece()).is_move_valid(stx, sty, destx, desty))
 		{
-			board_field[desty][destx].set_piece(new pawn(color::WHITE)); //TODO noch die eigenschaften der figur gettn
+			chess_piece* old_piece;
+			if (short_name == "")
+			{
+				old_piece = new empty(col);
+			}
+			else
+			{
+				if (short_name == "P")
+				{
+					old_piece = new pawn(col);
+				}
+				else
+				{
+					if (short_name == "N")
+					{
+						old_piece = new knight(col);
+					}
+					else
+					{
+						if (short_name == "B")
+						{
+							old_piece = new bishop(col);
+						}
+						else
+						{
+							if (short_name == "R")
+							{
+								old_piece = new rook(col);
+							}
+							else
+							{
+								if (short_name == "Q")
+								{
+									old_piece = new queen(col);
+								}
+								else
+								{
+									if (short_name == "K")
+									{
+										old_piece = new king(col);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			board_field[desty][destx].set_piece(old_piece); //TODO noch die eigenschaften der figur gettn
 			board_field[sty][stx].set_piece(new empty(color::NONE));
 			cout << "dest " << (*board_field[desty][destx].get_piece()).get_short_name() << "\n";
 			cout << "st " << (*board_field[sty][stx].get_piece()).get_short_name() << "\n";
