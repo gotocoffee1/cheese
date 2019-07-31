254d98ecf0f350faef312fc1951b3b7ec0c5f004
gotocoffee <gotocoffee1@gmail.com>
2019-07-31 20:03:29 +0200

    quick n dirty board

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -5,6 +5,28 @@ namespace cheese
 {
     void renderer::on_update(bool&)
     {
-        gn::Text("CHESS");
+        if (GN(Window, "Board"))
+        {
+            ImVec2 pos = gn::GetCursorScreenPos();
+            ImDrawList* draw_list = gn::GetWindowDrawList();
+
+            auto p = pos;
+            for (int i = 0; i < 8; ++i)
+            {
+                for (int j = 0; j < 8; ++j)
+                {
+                    ImU32 col;
+
+                    if ((i + j) % 2)
+                        col = IM_COL32(124, 75, 64, 255);
+                    else
+                        col = IM_COL32(228, 193, 151, 255);
+                    draw_list->AddRectFilled({ p.x, p.y }, { p.x + 50, p.y + 50 }, col);
+                    p.x += 50;
+                }
+                p.x = pos.x;
+                p.y += 50;
+            }
+        }
     }
 }
